8e690c24497402237692d158bab50d55eeee22fc
Lin <894696359@qq.com>
2023-08-01 21:52:02 +0800

    can use

--- new-breakout-game/src/App.vue
+++ new-breakout-game/src/App.vue
@@ -4,7 +4,6 @@ import inputBox from './components/inputBox.vue'
 </script>
 
 <template>
-    <h1 class="text-5xl text-[#fff] text-center">Breakout!</h1>
     <game/>
 </template>
 
---
+++ new-breakout-game/src/components/endBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50 bg-slate-500 bg-opacity-50" v-if="gameOver">
+    <div class="bg-white rounded-lg shadow-lg p-6">
+      <h3 class="font-bold text-lg">Congratulations!</h3>
+      <div class="py-4 text-lg font-bold">Your score is {{ score }}.</div>
+      <div class="flex justify-end">
+        <button class="btn"  @click="submitScore">Confirm</button>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: ['score','gameOver'],
+  emits: ['submit-score'],
+  methods: {
+    submitScore() {
+      this.$emit('submit-score', this.score);
+    },
+  },
+};
+</script>
+
